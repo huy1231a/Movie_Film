4e94bdaaee0bd9eac51f7f15e3779f8dd672ebe8
huy1231a <taolahuy2k0@gmail.com>
2024-03-19 17:09:52 +0700

    first init

---
+++ pages/auth.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div className='relative h-full w-full  bg-[url("/images/hero.jpg")]'>
+      <h1>Hao</h1>
+    </div>
+  )
+}
+
+export default Auth
